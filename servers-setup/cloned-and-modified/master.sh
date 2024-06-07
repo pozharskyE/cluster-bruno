4b1c1dbf08ea29833c83900933ee4e2031817302
pozharskye <evgeny.balashov.w@gmail.com>
2024-06-07 20:56:56 +0300

    changed default ip for master (private=>public)

--- servers-setup/cloned-and-modified/master.sh
+++ servers-setup/cloned-and-modified/master.sh
@@ -5,7 +5,7 @@
 
 # If you need public access to API server using the servers Public IP adress, change PUBLIC_IP_ACCESS to true.
 
-PUBLIC_IP_ACCESS="false"
+PUBLIC_IP_ACCESS="true"
 NODENAME=$(hostname -s)
 POD_CIDR="192.168.0.0/16"
 
